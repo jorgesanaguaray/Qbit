d08db4107638e8821f827c1e0af61e33f2883e64
jorgesanaguaray <jorgesanaguaray1@gmail.com>
2023-09-20 15:24:19 -0500

    Initial settings.

---
+++ app/src/main/java/com/jorgesanaguaray/qbit/MyApp.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.qbit
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class MyApp : Application()
