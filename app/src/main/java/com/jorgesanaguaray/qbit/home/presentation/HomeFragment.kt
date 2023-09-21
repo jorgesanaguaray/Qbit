19c7dd07d9c16b4a24e7e5f10905d8f713247d9d
jorgesanaguaray <jorgesanaguaray1@gmail.com>
2023-09-20 22:20:27 -0500

    I added a dialogue box to view the details of each post.

--- app/src/main/java/com/jorgesanaguaray/qbit/home/presentation/HomeFragment.kt
+++ app/src/main/java/com/jorgesanaguaray/qbit/home/presentation/HomeFragment.kt
@@ -1,16 +1,21 @@
 package com.jorgesanaguaray.qbit.home.presentation
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
 import coil.load
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.jorgesanaguaray.qbit.R
 import com.jorgesanaguaray.qbit.core.domain.Post
+import com.jorgesanaguaray.qbit.databinding.DialogDetailBinding
 import com.jorgesanaguaray.qbit.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,12 +28,15 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    private var bottomSheetDialog: BottomSheetDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         homeViewModel = ViewModelProvider(this).get()
         homeAdapter = HomeAdapter(
             onMoreClick = {
+                setupDetailDialog(it)
             },
             onPostClick = {
             }
@@ -66,9 +74,58 @@ class HomeFragment : Fragment() {
 
     }
 
+    override fun onPause() {
+        super.onPause()
+
+        hideDetailDialog()
+
+    }
+
     private fun setupRecyclerView(posts: List<Post>) {
         homeAdapter.setPosts(posts)
         binding.mRecyclerView.adapter = homeAdapter
+    }
+
+    private fun setupDetailDialog(post: Post) {
+
+        bottomSheetDialog = BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
+        val binding: DialogDetailBinding = DialogDetailBinding.inflate(layoutInflater)
+
+        binding.apply {
+
+            mTitle.text = post.title
+            mImage.load(post.imageLink) {
+                placeholder(R.drawable.progress_animation)
+                error(R.drawable.ic_error)
+                crossfade(true)
+                crossfade(400)
+            }
+            mCategory.text = HtmlCompat.fromHtml("<b>" + resources.getString(R.string.category_) + "</b>" + " " + post.category, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            mCreatedBy.text = HtmlCompat.fromHtml("<b>" + resources.getString(R.string.created_by_) + "</b>" + " " + post.createdBy, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            mDescription.text = post.description
+
+            mClose.setOnClickListener {
+                hideDetailDialog()
+            }
+            mSharePost.setOnClickListener {
+                sharePost(post.postLink)
+            }
+            mOpenInApp.setOnClickListener {
+            }
+            mOpenInBrowser.setOnClickListener {
+                openInBrowser(post.postLink)
+                hideDetailDialog()
+            }
+
+        }
+
+        bottomSheetDialog?.setContentView(binding.root)
+        bottomSheetDialog?.show()
+
+    }
+
+    private fun hideDetailDialog() {
+        bottomSheetDialog?.dismiss()
     }
 
     private fun setupViewsVisibility(isLoading: Boolean) {
@@ -81,6 +138,19 @@ class HomeFragment : Fragment() {
             binding.mNestedScroll.visibility = View.VISIBLE
         }
 
+    }
+
+    private fun sharePost(postLink: String) {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, postLink)
+        startActivity(Intent.createChooser(shareIntent, resources.getString(R.string.share)))
+    }
+
+    private fun openInBrowser(postLink: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(postLink)
+        startActivity(intent)
     }
 
     private fun goAddScreen() {
